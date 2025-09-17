6e7233b46e94a9375c95f50aceb8dea2a05eb02a
ovi1310 <ovidiu-ioan.nerges@estudiantat.upc.edu>
2025-09-17 09:22:22 +0200

    Exercicis github
    Exercicis finalitzats

--- Assets/Saludar.cs
+++ Assets/Saludar.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+
+    void SayHello()
+    {
+        Debug.Log("Hello world");
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SayHello();
     }
 
     // Update is called once per frame
