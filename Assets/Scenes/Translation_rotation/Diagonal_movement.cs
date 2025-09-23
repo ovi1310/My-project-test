5d3db3a98699682e9263fd2acecbfbfa947bf33d
ovi1310 <ovidiu-ioan.nerges@estudiantat.upc.edu>
2025-09-23 12:09:25 +0200

    hfdhn

--- Assets/Scenes/Translation_rotation/Diagonal_movement.cs
+++ Assets/Scenes/Translation_rotation/Diagonal_movement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    private 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
