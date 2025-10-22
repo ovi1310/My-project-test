11a07baf41781876f37864ee05faffa65ff0f6a7
ovi1310 <ovidiu-ioan.nerges@estudiantat.upc.edu>
2025-10-22 11:28:39 +0200

    c

--- Assets/Scenes/New input system/Cube.cs
+++ Assets/Scenes/New input system/Cube.cs
@@ -6,10 +6,19 @@ public class Cube : MonoBehaviour
     public Rigidbody2D rb; //Necessari per al moviment d'un objecte
     public float moveSpeed;
     private Vector2 _moveDirection;
+    private MeshRenderer myCube;
 
-    public InputActionReference movement; // Referència a l'acció que volem utilitzar
+    public InputActionReference movement; // Referï¿½ncia a l'acciï¿½ que volem utilitzar
     public InputActionReference colorChange;
 
+    private void Start()
+    {
+        myCube = GetComponent<MeshRenderer>();
+    }
+    private void OnEnable()
+    {
+        colorChange.action.performed += ctx => changeColor();
+    }
     void Update()
     {
         _moveDirection = movement.action.ReadValue<Vector2>();
@@ -17,6 +26,10 @@ public class Cube : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+        rb.linearVelocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+    }
+    void changeColor()
+    {
+        myCube.material.color = new Color(Random.value, Random.value, Random.value);
     }
 }
